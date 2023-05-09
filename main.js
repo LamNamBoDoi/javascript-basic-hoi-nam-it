3310d7c99baefe7ad0d2e48ede8ffb8992e1d7a5
Lam Nam <nguyenph.nam369@gmail.com>
2023-05-09 21:28:31 +0700

    source video 63

---
+++ main.js
@@ -0,0 +1,7 @@
+// var headingNode = document.getElementById("heading");
+//var headingNode = document.getElementsByClassName("heading");
+var headingNode = document.querySelector("html .box .heading-2");
+
+console.log({
+  element: headingNode,
+});

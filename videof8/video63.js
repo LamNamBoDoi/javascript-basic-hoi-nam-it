9738501aa28f230d8d66ba40d0a6429bcc368cfc
Lam Nam <nguyenph.nam369@gmail.com>
2023-05-10 04:51:51 +0700

    source video63

--- videof8/video63.js
+++ videof8/video63.js
@@ -8,3 +8,18 @@ var boxNode = document.querySelector(".box-1");
 console.log(boxNode);
 // Công việc 2: sử dụng tới các li elements con của .box-1
 console.log(boxNode.querySelectorAll("li"));
+
+// Công thức cần nhớ
+// 1. getElementById
+// 2. getElementsByClassName
+// 3. getElementsByTagName
+// 4. querySelector
+// 5. querySelectorAll
+
+// 1, 4 trả về 1 element
+// 2, 3 trả về HTMLCollection
+// 5 trả về nodelist
+
+// 6. HTML collection: img, form, a,...
+
+// 7. document.write
